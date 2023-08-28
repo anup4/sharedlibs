4c997704c10273a107f7ef0268da9648197f81a3
anup4 <62816576+anup4@users.noreply.github.com>
2023-08-28 13:47:18 +0530

    Update uploadToNexus.groovy

--- vars/uploadToNexus.groovy
+++ vars/uploadToNexus.groovy
@@ -1,1 +1,14 @@
-
+def call(nexusRepo) {
+   def pom = readMavenPom file: 'pom.xml'
+   def repo = nexusRepo //pom.version.endswith('SNAPSHOT') ? "my-app-snapshot" : "my-app-release"
+   def artifactid = pom.artifactid
+   def groupid = pom.groupid
+   nexusArtifactUploader artifacts: [[artifactId: artifactid , classifier: '', file: 'target/${artifactid}-${pom.version}.war', type: 'war']], 
+   credentialsId: 'AJ', 
+   groupId: groupid, 
+   nexusUrl: '172.31.43.180:8082', 
+   nexusVersion: 'nexus2', 
+   protocol: 'http', 
+   repository: repo, 
+   version: pom.version
+}
